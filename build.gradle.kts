853f14c4e80827445c6c2bbab2fd4a441c17d75a
tlm920 <timo@timomcgrath.com>
2024-10-04 21:34:04 -0400

    Fix deps and shadowjar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version("8.1.1")
+    id("com.gradleup.shadow") version "8.3.2"
     id("com.github.ben-manes.versions") version "0.48.0"
 }
 
@@ -18,6 +18,7 @@ version = "$majorVersion-$minorVersion"
 repositories {
     mavenCentral()
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://repo.glaremasters.me/repository/public/")
     maven("https://nexus.phoenixdevt.fr/repository/maven-public/")
@@ -29,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(libs.spigot)
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
     compileOnly(libs.vault)
     compileOnly(libs.authlib)
@@ -44,7 +45,6 @@ dependencies {
 
     implementation(libs.nashorn)
     implementation(libs.adventure.platform)
-    implementation(libs.adventure.minimessage)
 
     compileOnly("org.jetbrains:annotations:23.0.0")
     compileOnly("net.mvndicraft:mvndicore:2.0.0-SNAPSHOT")
@@ -57,9 +57,12 @@ tasks {
         relocate("net.kyori", "com.extendedclip.deluxemenus.libs.adventure")
         archiveFileName.set("MvndiMenus-${rootProject.version}.jar")
     }
+    build {
+        dependsOn(shadowJar)
+    }
     java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     processResources {
